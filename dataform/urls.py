1390cf6091d108f2f07c36f9fd7418b5366e1837
Pavel Piasetcki <pyasecky2010pavel@mail.ru>
2020-12-24 11:40:23 +0300

    Markup is ready.

--- dataform/urls.py
+++ dataform/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
-from bboard.views import index
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('', index),
-    path('bboard/', include('bboard.urls')),
-    path('admin/', admin.site.urls),
-    path('form/', include('dataform.urls')),
+    path('', views.DataFormView.as_view()),
+
 ]
