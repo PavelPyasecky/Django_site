a10aadb4af2b6aaea05a61a44f7dc8b946edd92a
Pavel Piasetcki <pyasecky2010pavel@mail.ru>
2021-01-05 13:11:11 +0300

    Some changes in settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -132,4 +133,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_RECEPIENT = os.getenv('EMAIL_RECEPIENT')
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', True)
+
+django_heroku.settings(locals())
 
